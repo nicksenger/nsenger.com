491d0eddd779fac84223b08d3f4a6d043e25a28f
Nick <dev@nsenger.com>
2019-09-23 21:15:57 -0700

    feat: basic project setup

---
+++ src/server.re
@@ -0,0 +1,28 @@
+/* make the express app */
+let app = Express.App.make();
+
+/* Our initial rendering function, we will soon make this way better */
+let renderHTML = (_next, _req, res) => {
+  let content = ReactDOMServerRe.renderToString(<Component2 greeting="Hello!" />);
+  Express.Response.sendString(content, res);
+};
+
+/* Express works on middleware and bs-express provides an easy way to make
+   middleware out of functions */
+renderHTML
+|> Express.Middleware.from
+|> Express.App.useOnPath(~path="/", app);
+
+let port = 3000;
+
+/* Getting a nice message when the server starts */
+let onListen = e =>
+  switch e {
+  | exception (Js.Exn.Error(e)) =>
+    Js.log(e);
+    Node.Process.exit(1);
+  | _ => Js.log("listening at localhost:" ++ string_of_int(port))
+  };
+
+/* starting up the express app */
+Express.App.listen(app, ~onListen, ~port, ());
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
+const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const outputDir = path.join(__dirname, 'build/');
-
-const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
-  output: {
-    path: outputDir,
-    filename: 'Index.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: false
-    })
-  ],
-  devServer: {
-    compress: true,
-    contentBase: outputDir,
-    port: process.env.PORT || 8000,
-    historyApiFallback: true
-  }
+    entry: {
+        bundle: ['./src/client/Index.bs.js', './src/client/styles/index.scss'],
+        sw: './src/client/sw.bs.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'src/client/dist'),
+        filename: '[name].js',
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['.js', '.scss']
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/ },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css',
+                        }
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'postcss-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    }
 };
